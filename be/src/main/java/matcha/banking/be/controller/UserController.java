78c1390dc57e5e337f41d580951a225f0b33900e
Vund <vund1@runsystem.net>
2024-05-01 22:47:45 +0700

    feat: register user

---
+++ be/src/main/java/matcha/banking/be/controller/UserController.java
@@ -0,0 +1,47 @@
+package matcha.banking.be.controller;
+
+import lombok.RequiredArgsConstructor;
+import matcha.banking.be.dto.RegisterDto;
+import matcha.banking.be.entity.UserEntity;
+import matcha.banking.be.service.UserService;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Object> createUser(@RequestBody RegisterDto registerDto) {
+        Map<String, Object> responseBody = new HashMap<>();
+        try {
+
+            UserEntity userEntity = userService.createUser(registerDto);
+
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{email}")
+                    .buildAndExpand(registerDto.getEmail())
+                    .toUri();
+
+            return ResponseEntity.created(location).body(userEntity);
+        } catch (IllegalArgumentException ie) {
+            responseBody.put("error", ie.getMessage());
+            return ResponseEntity.badRequest().body(responseBody);
+        } catch (DuplicateKeyException de) {
+            responseBody.put("error", de.getMessage());
+            return ResponseEntity.status(409).body(responseBody);
+        }
+    }
+}
